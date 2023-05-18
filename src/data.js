1ea6fa52c26ff2271873da5e064a23f7f54b4d2f
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-05-19 00:09:18 +0800

    update

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const myData = [
+    {
+        name:"Hairul Sulaiman",
+        age : 27,
+        role:"Front End Developer"
+    },
+    {
+        skill_1:"HTML",
+        skill_2:"CSS",
+        skill_3:"JAVASCRIPT",
+        skill_4:"BOOTSTRAP",
+        skill_5:"REACTJS",
+        skill_6:"TAILWIND CSS"
+    },
+    {
+        proj_1:'Bike Batallion',
+        link_1 :'www.bikebatallion.com',
+        proj_2:'Mommies Baby',
+        link_2 :'www.mommiesbaby.com'
+    }
+]
+
+export default myData
